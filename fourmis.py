2f865f75616fc9e73adb0c699b4e4874f3cf71d1
dinnasty <81336306+dinnasty@users.noreply.github.com>
2021-08-14 02:34:19 +0200

    Update fourmis.py

--- fourmis.py
+++ fourmis.py
@@ -3,7 +3,7 @@ import numpy as np
 import numpy.random as npr
 from utils import *
 
-''' Met à jour les phéromones : chemin plus court = plus de phéromones '''
+''' Updates pheromones : shorter path = more pheromones '''
 def update_pheromones(path, path_length, pheromone_matrix):
 
 	for i in range(0, len(pheromone_matrix)):
@@ -12,7 +12,7 @@ def update_pheromones(path, path_length, pheromone_matrix):
 
 		pheromone_matrix[pt1][pt2] += 1/path_length
 
-''' Pour un point donné, connaissant les points qui n\'ont pas encore été parcourus par la fourmi, calcule la probabilité pour chaque points'''
+''' Calculate the probability of the ant to go to a given possible next point '''
 def gen_probas(last_point, pt_left, pheromone_matrix, distance_matrix, a, b):
 	probas = []
 	total = 0
@@ -26,17 +26,17 @@ def gen_probas(last_point, pt_left, pheromone_matrix, distance_matrix, a, b):
 	probas = [proba / total for proba in probas] # On divise par la somme des probabilités pour obtenir un SCE (somme des probabilités = 1)
 	return probas
 
-''' Point suivant choisi par la fourmi en fonction des probabilités de chaque point'''
+''' Next point chosen by the ant given the probabilities '''
 def next_point(last_point, points_left, pheromone_matrix, distance_matrix, a, b):
 	batch = gen_probas(last_point, points_left, pheromone_matrix, distance_matrix, a, b)
 	return npr.choice(points_left, 1, p=batch)[0]
 
-''' Code le comportement de la fourmi : choisit les points un par un au hasard en tenant compte de la distance et des phéromones, en gardant en mémoire les points restants
-	De plus, la longueur du chemin est calculée au fur et à mesure de l'avancement de la fourmi, en utilisant des distances précalculées pour accélérer le programme
-	a et b sont les paramètres de réglage modifiant l'importance de la distance et des phéromones dans le choix de la fourmi '''
+''' Ant behavior : chooses points one by one and randomly while taking into account distance and pheromone levels. Stores in memory the points it hasn't yet been to.
+	Path length is calculated during the ant's travel, uses the precalculated distances.
+	a and b are parameters modifying the impact of distance or pheromones in the ant's behavior '''
 def fourmi(points, pheromone_matrix, distance_matrix, a, b):
 	nb_points = len(points)
-	path = [0] # le chemin sera construit dans cette liste. exemple de chemin : [0, 6, 4, 2, 3, 5, 1] (remarque : les chemins sont fermés : le dernier point est relié au premier)
+	path = [0] # path is build in this list. path list example : [0, 6, 4, 2, 3, 5, 1] (paths are closed : the last point is linked to the first)
 	points_left = list(points)
 	path_length = 0
 	for i in range(nb_points):
@@ -49,8 +49,8 @@ def fourmi(points, pheromone_matrix, distance_matrix, a, b):
 		path.append(n_p)
 	return path, path_length
 
-''' Simule nb_gen générations de fourmis, avec a = 4 et b = 1, et renvoie le meilleur chemin.
-	Remplit en plus deux graphes : longueur du chemin par génération et longueur du meilleur chemin trouvé jusqu'alors à la n-ième génération'''
+''' Simulate nb_gen generations of ants and returns the best path found, with a = 4 et b = 1.
+	two graphs : [length of path per generation] and [length of best found path before the nth generation]'''
 def generations(points, nb_gen, distance_matrix):
 	nb_points = len(points)
 	pheromone_matrix = np.ones((nb_points, nb_points))
@@ -77,7 +77,7 @@ def generations(points, nb_gen, distance_matrix):
 
 	return best_path, pheromone_matrix, distance_matrix, length_graph, best_length_graph
 
-''' Fonction principale, qui exécute generations() avec des paramètres de test, et affiche les graphes utiles '''
+''' main function that calls generations() with tests parameters and plots useful graphs '''
 def main():
 	nb_points = 20
 	nb_gen = 1000
@@ -90,15 +90,15 @@ def main():
 
 	display_path(path, points)
 	plot_points(points)
-	plt.title("Meilleur chemin")
+	plt.title("Best path")
 
 	plt.subplot(1, 2, 2)
 
-	plt.plot(list(range(nb_gen)), length_graph, label='Longueur du chemin')
-	plt.plot(list(range(nb_gen)), best_length_graph, label='Meilleur longueur')
+	plt.plot(list(range(nb_gen)), length_graph, label='Path length')
+	plt.plot(list(range(nb_gen)), best_length_graph, label='Best length')
 
 	plt.legend()
-	plt.title("Evolution de la longueur du chemin au cours des générations")
+	plt.title("Path length evolution during the different generations")
 	plt.show()
 
 main()
