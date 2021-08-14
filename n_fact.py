e57149173655d7642272e761148544b9c1d19821
dinnasty <81336306+dinnasty@users.noreply.github.com>
2021-08-14 02:14:23 +0200

    Update n_fact.py

--- n_fact.py
+++ n_fact.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from utils import *
 
 
-''' Génère en utilisant la récursivité une liste de tous les chemins possibles, commençant par 0.
-	Pour chaque point, on ajoute tous les chemins commençant par ce point -> appel à get_all_path sur l'ensemble des points privé du point choisi
-	fp permet de fixer le premier point à 0
-	Cette fonction calcule en même temps la longueur de chaque chemin
-	-> renvoie la liste des couples (path_i, length_i), avec i variant de 1 à (n-1)! (car nombre de permutations sur l'ensemble [1, n-1]'''
+''' Recursively generates the list of all possible paths, starting with 0:
+	For each point, adds all paths starting by this point -> calls get_all_path on the set of all points except the chosen point
+	fp allows to force the first point to be 0
+	Functions calculates the length of the path as well
+	-> return the pair (path_i, length_i) with i going from 1 to (n-1)! (number of permutation of the set [1, n-1]'''
 def get_all_paths(inds, dists, points, fp = False):
 	n = len(inds)
 	if(n == 0): return [([], 0)]
@@ -27,14 +27,16 @@ def get_all_paths(inds, dists, points, fp = False):
 				paths.append([[p] + path[0], d + path[1]])
 	return paths
 
-''' Enfin, fonction qui sélectionne le chemin le plus court, par un simple appel à min() '''
+''' Selects the best path'''
 def get_best_path(inds, dists, points):
         paths = get_all_paths(inds, dists, points, True)
         best_path = min(paths, key = lambda p: p[1]) # p[1] correction à l'élément length de (path, length)
 
         return best_path
 
+''' Set of predetermined points for testing'''
 points = [(-0.464, -0.48), (-0.106, -0.155), (-0.484, 0.2225), (0.12, -0.66), (0.308, 0.275), (-0.004, 0.5), (0.51, -0.2525), (-0.4, -0.1825), (0.03, -0.135)]
+''' Set of 9 random points'''
 #points = gen_points(9)
 
 dists = create_distance_matrix(points)
